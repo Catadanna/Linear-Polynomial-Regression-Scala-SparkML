c7d37c8daf9580c6e56bff40b25b2c9bd682c8df
Catalina Chircu <35842763+Catadanna@users.noreply.github.com>
2019-07-02 11:39:43 +0200

    Add files via upload
    Functions, among others:
    Convert DataFrame to DenseMatrix
    Convert DataFrame to DenseVector
    Convert DataFrame to RDD

--- Tools.scala
+++ Tools.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.evaluation.RegressionMetrics
+import org.apache.spark.mllib.linalg.{DenseMatrix, DenseVector}
 import org.apache.spark.rdd.RDD
 
 object Tools {
@@ -45,5 +46,61 @@ object Tools {
 
     Math.sqrt(rm.meanSquaredError)
   }
+
+  /**
+    * Transforms a DataFrame into a DenseMatrix
+    * @param featuresDF
+    * @return
+    */
+  def getDenseMatrixFromDF(featuresDF:DataFrame):DenseMatrix = {
+    val featuresTrain = featuresDF.columns
+    val rows = featuresDF.count().toInt
+
+    val newFeatureArray:Array[Double] =
+      featuresTrain
+        .indices
+        .flatMap(i => featuresDF
+          .select(featuresTrain(i))
+          .collect())
+        .map(r => r.toSeq.toArray).toArray.flatten.flatMap(_.asInstanceOf[org.apache.spark.ml.linalg.DenseVector].values)
+
+    val flatArray = newFeatureArray
+    val newCols = flatArray.length / rows
+    val denseMat:DenseMatrix = new DenseMatrix(rows, newCols, flatArray, isTransposed=false)
+    denseMat
+  }
+
+  /**
+    * Transforms a DataFrame into a DenseVector
+    * @param featuresDF
+    * @return
+    */
+
+  def getDenseVectorFromDF(featuresDF:DataFrame):DenseVector = {
+    val featuresTrain = featuresDF.columns
+    val cols = featuresDF.columns.length
+
+    cols match {
+      case i if i>1 => throw new IllegalArgumentException
+      case _ => {
+        def addArray(acc:Array[Array[Double]], cur:Array[Double]):Array[Array[Double]] = {
+          acc :+ cur
+        }
+
+        val newFeatureArray:Array[Double] = featuresTrain
+          .indices
+          .flatMap(i => featuresDF
+            .select(featuresTrain(i))
+            .collect())
+          .map(r => r.toSeq.toArray.map(e => e.asInstanceOf[Double])).toArray.flatten
+
+
+        val denseVec:DenseVector = new DenseVector(newFeatureArray)
+        denseVec
+      }
+    }
+  }
+
+
 
 }
